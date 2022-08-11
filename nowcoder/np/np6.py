ffc84909af2867cbfa53d52a04c9d1380fea4c69
lihoufeng <lihoufeng@52tt.com>
2022-08-11 08:05:59 +0800

    auto update 2022-08-11 08:05:59

---
+++ nowcoder/np/np6.py
@@ -0,0 +1,4 @@
+# https://www.nowcoder.com/practice/0454d1bd28cd499ba06afdabe0fb9caf
+# NP6 牛牛的小数输出
+num = float(input())
+print("%.2f"%num)
---
+++ nowcoder/np/np7.py
@@ -0,0 +1,4 @@
+# https://www.nowcoder.com/practice/e302a62c1fbd4bbf84793e8c52ce4705
+# NP7 小数化整数
+num=float(input())
+print(int(num))
